a62374e96614ff97c16be5b43b717afe697d9dcb
shabby_boy <925750301@qq.com>
2018-08-29 21:42:30 +0800

    在celery中配置下队列，以便扩展

--- celeryDemo/config.py
+++ celeryDemo/config.py
@@ -1,4 +1,13 @@
 from __future__ import absolute_import
-
-#CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/5'
+from kombu import Exchange,Queue
+#CELERY_RESULT_BACKEND = 'redis://:abc@192.168.0.101:6379/2'
 BROKER_URL = 'redis://:abc@192.168.0.101:6379/1'
+
+CELERY_QUEUES = (
+    Queue("default",Exchange("default"),routing_key="default"),
+    Queue("task_A",Exchange("task_A"),routing_key="task_a")
+)
+
+CELERY_ROUTES={
+    'celeryDemo.tasks.spider':{"queue":"task_A","routing_key":"task_a"}
+}
